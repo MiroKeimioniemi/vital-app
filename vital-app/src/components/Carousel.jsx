66f32348fbae370cc9fef74a35bc677a0ef7d762
Miro Keimiöniemi <miro.keimioniemi@aalto.fi>
2023-11-11 14:38:42 +0200

    Created a vite react project and made a carousel component

---
+++ vital-app/src/components/Carousel.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const SwipeableCarousel = ({ images }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div className="swipeable-carousel">
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <div key={index} className="carousel-item">
+            <img src={image} alt={`Slide ${index + 1}`} />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default SwipeableCarousel;
